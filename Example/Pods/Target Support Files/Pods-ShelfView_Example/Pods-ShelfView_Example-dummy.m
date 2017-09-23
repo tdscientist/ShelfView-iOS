0f3c0cfa72bf406ca7cd35dc10611f8db7dc8df7
tdscientist <tdscientist@gmail.com>
2017-09-23 17:59:42 +0100

    feat: create bare library via pod

---
+++ Example/Pods/Target Support Files/Pods-ShelfView_Example/Pods-ShelfView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShelfView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ShelfView_Example
+@end
---
+++ Example/Pods/Target Support Files/ShelfView/ShelfView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ShelfView : NSObject
+@end
+@implementation PodsDummy_ShelfView
+@end
